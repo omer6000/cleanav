01f08803dfa3c6257391666f9f59f877dc635f9d
omer6000 <omeriqbal7@hotmail.com>
2023-12-01 13:02:45 +0100

    output function progress

--- src/navigation.rs
+++ src/navigation.rs
@@ -3,15 +3,20 @@ use std::str::FromStr;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Cell {
-    Free,
-    Trash,
+    Free, // .
+    Trash, // X
+    Buoy, // O
 }
 
 #[derive(Clone, Debug)]
 pub struct State {
     x: usize,
     y: usize,
-    steps: usize
+    steps: usize,
+    buoy: u16, //count
+    free: u16, //count
+    trash: u16, //count
+    lastcell: Cell
 }
 
 impl State {
@@ -19,7 +24,11 @@ impl State {
         Self {
             x: 0,
             y: 0,
-            steps: 0
+            steps: 0,
+            buoy: 0,
+            free: 0,
+            trash: 0,
+            lastcell: Cell::Free,
         }
     }
 }
@@ -33,7 +42,7 @@ impl TryFrom<char> for Cell {
         } else if c == 'X' {
             Cell::Trash
         } else {
-            unimplemented!()
+            Cell::Buoy
         })
     }
 }
@@ -43,6 +52,7 @@ impl From<Cell> for char {
         match cell {
             Cell::Free => '.',
             Cell::Trash => 'X',
+            Cell::Buoy => 'O',
         }
     }
 }
@@ -64,20 +74,18 @@ pub(crate) fn terminal(termination: &Termination, map: &crate::map::Map, state: 
                 return true;
             }
         }
-        // if i == state.steps break 
-        // println!("{}",x),
     };
     return false;
-    // todo!()
-    // let termination = Termination::Steps(steps)   {
-    //     println!("Terminate after {} steps", steps);
-    // }
-    // return true
-    // if 
 }
 
 pub enum Output {
     Position,
+    Steps,
+    TerminalSymbol,
+    DistinctSymbols,
+    Trash,
+    Free,
+    Buoy,
 }
 
 impl FromStr for Output {
@@ -96,42 +104,68 @@ where
         let text: &str = text.into();
         match text.to_lowercase().as_str() {
             "p" => Output::Position,
+            "n" => Output::Steps,
+            "s" => Output::TerminalSymbol,
+            "d" => Output::DistinctSymbols,
+            "c,x" => Output::Trash,
+            "c,." => Output::Free,
+            "c,o" => Output::Buoy,
             _ => panic!("{} is not supported", text),
         }
     }
 }
 
 pub(crate) fn output(output: &Output, end: (crate::map::Map, State)) -> String {
-    // todo!();
-    // println!("{}",output.into());
-    // return String::from("");
-    // println!("Output: {:?}", output);
-    // return "".to_string();
-    // match output {
-    //     Output::Position => println!("Helloo"),
-    // };
-    // match output{
-    //     Output::Position => print!("{}",Position),
-    // }
-    return "abc".to_string()
+    match output {
+        Output::Steps => return end.1.steps.to_string(),
+        Output::DistinctSymbols => {
+            let mut distinct: u8 = 0;
+            if end.1.buoy != 0 {
+                distinct += 1;
+            }
+            if end.1.free != 0 {
+                distinct += 1;
+            }
+            if end.1.trash != 0 {
+                distinct += 1;
+            }
+            return distinct.to_string();
+        },
+        Output::TerminalSymbol => {
+            match end.1.lastcell {
+                Cell::Free => return '.'.to_string(),
+                Cell::Trash => return 'X'.to_string(),
+                Cell::Buoy => return 'O'.to_string(),
+            }
+        }
+        Output::Position => return format!("({},{})", end.1.x,end.1.y),
+        Output::Trash => return end.1.trash.to_string(),
+        Output::Free => return end.1.free.to_string(),
+        Output::Buoy => return end.1.buoy.to_string(),
+
+    };
 }
 
 pub(crate) fn step(map: &mut crate::map::Map, state: &mut State, slope: (usize, usize)) -> () {
-    // todo!()
-    // let (x,y) = &slope.;
     state.steps += 1;
-    state.x += slope.0;
-    state.y += slope.1;
+    state.x = (state.x + slope.0) % map.width();
+    state.y = (state.y + slope.1) % map.height();
 
     let cell = map.get((state.x,state.y));
     match cell {
-        Cell::Trash => map.clean((state.x,state.y)),
-        Cell::Free => {},
+        Cell::Trash => {
+            map.clean((state.x,state.y));
+            state.trash += 1;
+            state.lastcell = Cell::Trash;
+        },
+        Cell::Free => {
+            state.free += 1;
+            state.lastcell = Cell::Free;
+        },
+        Cell::Buoy => {
+            state.buoy += 1;
+            state.lastcell = Cell::Buoy;
+        },
+    };
 
-    }
-
-    // println!("Step!!");
-    // println!("{} {}",x,y);
-    // return ();
-    // return (slope[0],slope[1]);
 }
