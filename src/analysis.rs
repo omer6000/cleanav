35f2288f1b2798e6c51ec56ffbc03cb46c147759
Omer Iqbal <omeriqbal7@hotmail.com>
2023-12-05 00:16:38 +0100

    transition probability

--- src/analysis.rs
+++ src/analysis.rs
@@ -10,10 +10,12 @@ pub fn generate_transition_matrix(width: usize, height: usize) -> DMatrix<f64> {
     
     for i in 0..width {
         for j in 0..height {
-            let state_index = i*height+j; //converting 2d index to 1d
+            // let state_index = i*height+j; //converting 2d index to 1d
+            let state_index = crate::markov::flatten_coordinate((i,j), width);
             for m in &moves {
                 let step = crate::markov::compute_step((i,j), (m.0,m.1), (width,height));
-                let step_index = step.0*height+step.1;
+                // let step_index = step.0*height+step.1;
+                let step_index = crate::markov::flatten_coordinate((step.0,step.1), width);
                 t_matrix[(state_index,step_index)] = t_matrix[(state_index,step_index)] + 1.0/17.0;
             }
         }
@@ -32,7 +34,14 @@ impl crate::markov::StochasticModel {
         steps: usize,
     ) -> RowDVector<f64> {
         // Hint: s_{t+1} = s_t * P
-        todo!()
+        // todo!()
+        let mut state = RowDVector::zeros(self.width*self.height);
+        state[crate::markov::flatten_coordinate(start,self.width)] = 1.0;
+        let tm = &self.transition_matrix;
+        for _ in 0..steps {
+            state *= tm;
+        }
+        return state;
     }
 
     pub fn compute_transition_probability(
@@ -42,12 +51,41 @@ impl crate::markov::StochasticModel {
         steps: usize,
     ) -> f64 {
         // Hint: use compute_state_distribution
-        todo!()
+        let end_index = crate::markov::flatten_coordinate(end, self.width);
+        return self.compute_state_distribution(start, steps)[end_index];
     }
 
     pub fn manhattan_distance(&self, from: (usize, usize), to: (usize, usize)) -> f64 {
         // Hint: The obvious path might not be the fastest (keep position wrapping in mind)
-        todo!()
+        // todo!()
+        let mut x_diff = (from.0 - to.0) as f64;
+        let mut y_diff = (from.1 - to.1) as f64;
+        if x_diff < 0.0 {
+            x_diff *= -1.0;
+        }
+        if y_diff < 0.0 {
+            y_diff *= -1.0;
+        }
+        let d1 =  x_diff + y_diff;
+
+        // Also cosidering using negative coordinates
+        let neg_to = ((to.0 - self.width) as f64,(to.1 - self.height) as f64);
+        let mut x_diff2 = from.0 as f64 - neg_to.0;
+        let mut y_diff2 = from.1 as f64 - neg_to.1;
+        if x_diff2 < 0.0 {
+            x_diff2 *= -1.0;
+        }
+        if y_diff2 < 0.0 {
+            y_diff2 *= -1.0;
+        }
+
+        let d2 = x_diff2 + y_diff2;
+        if d1 < d2 {
+            return d1;
+        }
+        else {
+            return d2;
+        }
     }
 
     pub fn expected_distance(&self, start: (usize, usize), steps: usize) -> f64 {
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,45 @@ use cleanav::tui;
 use std::str::FromStr;
 
 fn main() {
-    let tui::Opt {
-        slope,
-        termination,
-        map: map_path,
-        output,
-    } = tui::parse_args();
-    let map = Map::from_path(&map_path);
-    // println!("{}",output);
-    let result = run(
-        map,
-        slope,
-        termination.into(),
-        Output::from_str(&output).unwrap(),
-    );
-    tui::output(&result)
+    // let tui::Opt {
+    //     slope,
+    //     termination,
+    //     map: map_path,
+    //     output,
+    // } = tui::parse_args();
+    // let map = Map::from_path(&map_path);
+    // let result = run(
+    //     map,
+    //     slope,
+    //     termination.into(),
+    //     Output::from_str(&output).unwrap(),
+    // );
+    // tui::output(&result)
+    let width = 5;
+    let height = 4;
+    let start = (1,1);
+    let end = (2,2);
+    let tm = cleanav::analysis::generate_transition_matrix(width, height);
+    let model = cleanav::markov::StochasticModel {
+        transition_matrix: tm,
+        width: width,
+        height: height
+    };
+    // let vec = model.compute_state_distribution((0,0),5);
+    println!("{:?}",model.compute_transition_probability(start, end, 3));
+
+    // use nalgebra::{Matrix4, Vector4, U2, U3};
+
+// fn main() {
+//     let s = Vector4::new(1.0, 0.0, 0.0, 0.0).transpose();
+
+//     let p = Matrix4::new(
+//         0.6, 0.2, 0.15, 0.05, 0.64, 0.16, 0.16, 0.04, 0.45, 0.15, 0.3, 0.1, 0.48, 0.12, 0.32, 0.08,
+//     );
+
+//     let mut p_lim = s;
+//     for _ in 0..100 {
+//         p_lim = p_lim * p;
+//     }
+//     println!("Final probabilities: {:?}", p.shape());
 }
